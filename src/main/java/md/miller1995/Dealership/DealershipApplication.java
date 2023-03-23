8666fe6c12d1f07bba979f0e3b74b2b74cfe50b1
miller1995 <nircaanton95@gmail.com>
2023-03-23 14:38:40 +0200

    build a empty project for a web application car dealership

---
+++ src/main/java/md/miller1995/Dealership/DealershipApplication.java
@@ -0,0 +1,13 @@
+package md.miller1995.Dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealershipApplication.class, args);
+	}
+
+}
