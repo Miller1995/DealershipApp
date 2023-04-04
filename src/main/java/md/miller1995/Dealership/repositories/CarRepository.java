729bf8ba47313194e3ec7d21823b45742f1d72e5
miller1995 <nircaanton95@gmail.com>
2023-04-04 20:59:20 +0300

    - add new package repositories; - in this package add new interface CarRepository that extends JPARepository and this repository has all necessary method() for work with database; - add new annotation @Repository;

---
+++ src/main/java/md/miller1995/Dealership/repositories/CarRepository.java
@@ -0,0 +1,11 @@
+package md.miller1995.Dealership.repositories;
+
+import md.miller1995.Dealership.models.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car,Long> {
+
+    // TODO custom query if is necessary
+}
