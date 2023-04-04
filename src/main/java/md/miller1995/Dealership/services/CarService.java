e813e5cd180c64809e2d58477cf37e3a4a011ff5
miller1995 <nircaanton95@gmail.com>
2023-04-04 21:18:05 +0300

    - add method that return one car by id used jpa repository and method findById(); - this method return Optional, if car was found return car else return null;

--- src/main/java/md/miller1995/Dealership/services/CarService.java
+++ src/main/java/md/miller1995/Dealership/services/CarService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,5 +22,10 @@ public class CarService {
 
     public List<Car> findAllCar(){
         return carRepository.findAll();
+    }
+
+    public Car findOneCar(long id){
+        Optional<Car> foundCar = carRepository.findById(id);
+        return foundCar.orElse(null);
     }
 }
