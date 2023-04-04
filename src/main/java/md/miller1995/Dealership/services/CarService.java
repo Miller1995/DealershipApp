cce93d1fe8575a2b8a1963721e47445d78f057b2
miller1995 <nircaanton95@gmail.com>
2023-04-04 21:13:18 +0300

    - add method that return all cars from database used jpa repository and his method findAll();

--- src/main/java/md/miller1995/Dealership/services/CarService.java
+++ src/main/java/md/miller1995/Dealership/services/CarService.java
@@ -1,9 +1,12 @@
 package md.miller1995.Dealership.services;
 
+import md.miller1995.Dealership.models.Car;
 import md.miller1995.Dealership.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -14,5 +17,9 @@ public class CarService {
     @Autowired
     public CarService(CarRepository carRepository) {
         this.carRepository = carRepository;
+    }
+
+    public List<Car> findAllCar(){
+        return carRepository.findAll();
     }
 }
